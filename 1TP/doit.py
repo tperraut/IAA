7a01ade87c8eb5a234bd3b88488a4d6c9de6acee
Thomas Perraut <tperrau@a-018249.ups.u-psud.fr>
2017-01-05 10:36:58 +0100

    q2

--- 1TP/doit.py
+++ 1TP/doit.py
@@ -27,11 +27,40 @@ def treat_film(file_name):
     """
     recent = 0
     old = 0
-    film_dico = {}
-    #TODO: extract id films date note
-    for l in file_name.fp:
-        temp = l.split("|")
-        
+    dico = {}
+    for line in file_name.fp:
+        #ID|TITLE|GRADE
+        temp = line.split("|")
+        #TITLE (DATE)
+        data = temp[1].split("(")
+        date = int(data[1][0:4])
+        grade = int(temp[2][0:1])
+        print temp
+        print data
+        print date
+        print grade
+        if dico.has_key(data[0]):
+            dico[data[0]] = dico[data[0]].append((temp[0], grade, date))
+        else:
+            dico[data[0]] = [(temp[0], grade, date)]
+        if recent is 0:
+            recent = date
+        if old is 0:
+            old = date
+        old = min(old, date)
+        recent = max(recent, date)
+    return (dico, old, recent)
 
-#1
-print count_jugment(parse())
+def main():
+    """
+        main function
+    """
+    #1
+    print count_jugment(parse())
+    #2
+    (dico, old, recent) = treat_film(parse())
+    print "films: {3}\noldest: {1}\nmost recent: {2}\n".format(old,
+                                                               recent,
+                                                               len(dico.keys()))
+
+main()
